1ca3c8a602fc10befd1fcf54cd661a826ce3634a
mikoslaf <mikolaj2003wojcik@gmail.com>
2025-09-21 20:41:52 +0200

    Add SQL dump for users table and create .env.example file for database configuration

--- ExampleDB.sql
+++ ExampleDB.sql
@@ -1,6 +1,60 @@
-CREATE TABLE Users (
-    id CHAR(36) PRIMARY KEY DEFAULT UUID(),
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    age INTEGER NOT NULL
-)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Wrz 21, 2025 at 08:39 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exponet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` char(36) NOT NULL DEFAULT uuid(),
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `age`) VALUES
+('1d7980c0-971a-11f0-b843-7c10c987b3ee', 'Example1', 'Example1@Example.com', 33),
+('25326cec-971a-11f0-b843-7c10c987b3ee', 'Example2', 'Example2@Example.com', 55),
+('2b87d401-971a-11f0-b843-7c10c987b3ee', 'Example3', 'Example3@Example.com', 88);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`) USING HASH;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
